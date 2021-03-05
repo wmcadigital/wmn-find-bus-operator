c42a8db293dcb99359002dc4f98bfa1451facd5d
houbly <h.blyden@gmail.com>
2021-03-05 14:17:17 +0000

    add back button

--- src/components/FindBusRoute/FindBusRoute.js
+++ src/components/FindBusRoute/FindBusRoute.js
@@ -5,7 +5,12 @@ import Button from '../shared/Button/Button';
 import s from './FindBusRoute.module.scss';
 
 const FindBusRoute = () => {
-  const [autoCompleteState] = useContext(AutoCompleteContext);
+  const [autoCompleteState, autoCompleteDispatch] = useContext(AutoCompleteContext);
+  const resetSearch = () => {
+    autoCompleteDispatch({
+      type: 'RESET_SELECTED_ITEM',
+    });
+  };
   const { selectedItem } = autoCompleteState;
   return (
     <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg wmnds-grid">
@@ -14,6 +19,11 @@ const FindBusRoute = () => {
         <div className="wmnds-grid wmnds-grid--spacing-md-2-lg">
           {selectedItem.id ? (
             <div className="wmnds-col-md-2-3">
+              <Button
+                btnClass="wmnds-btn--link wmnds-m-none"
+                text="&lt; Back"
+                onClick={resetSearch}
+              />
               <h1>Your bus company is {selectedItem.operator.Name}</h1>
               <p>
                 If you are only travelling on {selectedItem.operator.Name} buses, you can buy
