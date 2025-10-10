640c4b3968804680de0b5d3aad6fdb29e59ad8b3
GeorgeEverett <George.Everett@wmca.org.uk>
2025-10-10 15:15:26 +0100

    Replace footer and logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,6 @@ import ContextProvider from 'globalState/ContextProvider';
 import FindBusRoute from '../FindBusRoute/FindBusRoute';
 
 function App() {
-  useEffect(() => {
-    document.getElementById('year').innerHTML = new Date().getFullYear();
-  }, []);
-
   return (
     <React.StrictMode>
       <ContextProvider>
