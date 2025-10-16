ccca682f0129e4d59f69a44796aba074d731868d
GeorgeEverett <George.Everett@wmca.org.uk>
2025-10-16 13:08:30 +0100

    fix import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ContextProvider from 'globalState/ContextProvider';
 import FindBusRoute from '../FindBusRoute/FindBusRoute';
 
